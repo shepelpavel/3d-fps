406003361bb2672387163ef329b1de17f6f1bd2d
Shepel Pavel <shepelpavel@mail.ru>
2023-12-10 17:46:56 +0300

    feat: fix paths 2

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
-const path = require('path')
 
 module.exports = defineConfig({
-  outputDir: path.resolve('./docs/'),
-  assetsDir: './3d-fps/',
+  outputDir: './docs',
+  publicPath: '/3d-fps/',
   transpileDependencies: true,
 })
